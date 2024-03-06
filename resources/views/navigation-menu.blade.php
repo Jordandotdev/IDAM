c4e74989f14a289284dd64e844ad12a414929fbd
jordandotdev <jordanjdyydj@gmail.com>
2024-03-06 22:29:38 +0530

    addition of the welcome route

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -5,15 +5,15 @@
             <div class="flex">
 
                 <!-- Logo -->
-                    <div class="shrink-0 flex items-center">
-                        <a href="{{ route('dashboard') }}" class="flex items-center">
-                            <img class="block h-9 w-auto" src="{{ asset('assets/Logo_IDAM.jpg') }}" alt="Application Logo" />
-                            <div class="ml-2 text-2xl font-semibold text-gray-900">
-                                <div>IDAM</div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="pt-2">&copy;</div>
+                <div class="shrink-0 flex items-center">
+                    <a href="{{ route('welcome') }}" class="flex items-center">
+                        <img class="block h-9 w-auto" src="{{ asset('assets/Logo_IDAM.jpg') }}" alt="Application Logo" />
+                        <div class="ml-2 text-2xl font-semibold text-gray-900">
+                            <div>IDAM</div>
+                        </div>
+                    </a>
+                </div>
+                <div class="pt-2">&copy;</div>
                 
 
                 <!-- Navigation Links -->
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
-});
+})->name('welcome');
 
 Route::middleware([
     'auth:sanctum',
